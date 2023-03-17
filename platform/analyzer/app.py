76a374d9ced5406a1f6030f9ff593c8dbaba16cf
StoyanTinchev <st8tinchev@gmail.com>
2023-03-17 23:33:15 +0200

    Issue: #3 Add: "props.env" file which will store all sensitive user information, and it will load our env variables via "dotenv" package.

--- platform/analyzer/app.py
+++ platform/analyzer/app.py
@@ -1,20 +1,22 @@
 import json
-from time import sleep
-
+import os
 from flask import Flask, request, jsonify
 import openai
+from dotenv import load_dotenv
+
+# This line loads the values from the .env file into the environment
+load_dotenv(
+    dotenv_path=os.path.join(os.path.dirname(__file__), 'props.env')
+)
 
 app = Flask(__name__)
-
-a = 0
+openai.api_key = os.getenv('OPENAI_SECRET_KEY', 'default-secret-key')
 
 
 @app.route('/travelerio/api/v1.0/hello', methods=['GET'])
-def openai_communication():  # put application's code here
-    global a
-    a += 1
-    sleep(3)
-    return jsonify({'message': str(a)})
+def openai_communication():
+    # for test purposes
+    return jsonify({'message': os.getenv('OPENAI_SECRET_KEY', 'default-secret-key')})
 
 
 if __name__ == '__main__':
