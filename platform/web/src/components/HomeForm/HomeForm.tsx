e2474ae73c81ce0593f74a2be055c0585cd9bfac
VasilPaligorov <vpaligorov@gmail.com>
2023-03-18 00:27:54 +0200

    Add: Current state of the project

--- platform/web/src/components/HomeForm/HomeForm.tsx
+++ platform/web/src/components/HomeForm/HomeForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FloatingLabel, Form, Row, Col, Button} from 'react-bootstrap';
+import { FloatingLabel, Form, Row, Col, Button } from 'react-bootstrap';
 
 interface Props {
     description: string;
@@ -8,27 +8,20 @@ interface Props {
     onButtonClick: Function;
 }
 
-const HomeForm: React.FC<Props> = ({description, setDescription, onButtonClick}) => {
+const HomeForm: React.FC<Props> = ({ description, setDescription, onButtonClick }) => {
     return (
-        // <Form>
-        //     <Form.Group className="mb-3" controlId="formBasicDescription">
-        //         <Form.Label>Description</HomeForm.Label>
-        //         <Form.Control type="text" placeholder="Enter Description" value={description} onChange={(event)=>setDescription(event.target.value)}/>
-        //     </Form.Group>
-        //     <Button variant="primary" type="submit" onClick={()=>onButtonClick()}>
-        //         Submit
-        //     </Button>
-        // </Form>
-
-
         <Form>
             <FloatingLabel
                 controlId="floatingTextarea"
                 label="Description"
                 className="mb-3"
             >
-                <Form.Control type="text" placeholder="Enter Description" value={description} onChange={(event)=>setDescription(event.target.value)}/>
-                <Button variant="primary" type="submit" onClick={(e)=>onButtonClick(e)}>
+                <Form.Control type="text" placeholder="Enter Description" value={description} onInput={(event: React.FormEvent<HTMLInputElement>) => setDescription(event.currentTarget.value)} />
+                <Form.Control type="number" placeholder="Enter Max price" value={description} onInput={(event: React.FormEvent<HTMLInputElement>) => window.alert(event.currentTarget.value)} />
+                <Form.Control type="text" placeholder="Eneter city/Country" value={description} onInput={(event: React.FormEvent<HTMLInputElement>) => window.alert(event.currentTarget.value)} />
+                <Form.Control type="date" placeholder="Enter date1" value={description} onInput={(event: React.FormEvent<HTMLInputElement>) => window.alert(event.currentTarget.value)} />
+                <Form.Control type="date" placeholder="Enter date2" value={description} onInput={(event: React.FormEvent<HTMLInputElement>) => window.alert(event.currentTarget.value)} />
+                <Button variant="primary" type="submit" onClick={(event: React.MouseEvent) => onButtonClick(event)}>
                     Submit
                 </Button>
             </FloatingLabel>
