2b0179a1ce0a35c85547f7fd8b459bd357e43edd
DaniRogachev <darogachev03@gmail.com>
2023-03-19 00:52:15 +0200

    Add loading screen to the project

---
+++ platform/web/src/DTO/LoadingItemDTO.ts
@@ -0,0 +1,6 @@
+export interface LoadingItemDTO{
+    imageSource: string;
+    city: string;
+    description: string;
+    suggestions: string[];
+}
--- platform/web/src/components/HomeForm/HomeForm.tsx
+++ platform/web/src/components/HomeForm/HomeForm.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { FloatingLabel, Form, Button, Row, Col } from 'react-bootstrap';
-import { HomeFormDTO } from '../../DTO/HomeFormDTO';
-import { postReq } from '../../services/makeRequests';
+import React, {useState} from 'react';
+import {FloatingLabel, Form, Button, Row, Col} from 'react-bootstrap';
+import {HomeFormDTO} from '../../DTO/HomeFormDTO';
+import {postReq} from '../../services/makeRequests';
 import './styles.css';
 
 
-const HomeForm = () => {
+const HomeForm = ({setShowLoading}: { setShowLoading: React.Dispatch<React.SetStateAction<boolean>> }) => {
     const [vacationDescription, setVacationDescription] = useState('');
     const [maxPrice, setMaxPrice] = useState<number>();
     const [currentCity, setCurrentCity] = useState('');
@@ -25,12 +25,16 @@ const HomeForm = () => {
             checkOutDate,
         };
 
+        setShowLoading(true);
 
         const [data, err] = await postReq('messages', formData);
 
+
         if (err) {
+            setShowLoading(false)
             console.log(err);
         } else {
+            setShowLoading(false)
             console.log(data);
         }
     };
@@ -45,29 +49,35 @@ const HomeForm = () => {
             <Row className=''>
                 <Col className=''>
                     <FloatingLabel label="Enter max price of the vacation">
-                        <Form.Control className='input' placeholder=' ' type="number" value={maxPrice} onInput={(event: React.FormEvent<HTMLInputElement>) => setMaxPrice(parseFloat(event.currentTarget.value))} />
+                        <Form.Control className='input' placeholder=' ' type="number" value={maxPrice}
+                                      onInput={(event: React.FormEvent<HTMLInputElement>) => setMaxPrice(parseFloat(event.currentTarget.value))}/>
                     </FloatingLabel>
                     <FloatingLabel label="Enter City/Country of departure">
-                        <Form.Control className='input' placeholder=' ' type="text" value={`${currentCity || ''}/${currentCountry || ''}`} onInput={(event: React.FormEvent<HTMLInputElement>) => modifyCity(event)} />
+                        <Form.Control className='input' placeholder=' ' type="text"
+                                      value={`${currentCity || ''}/${currentCountry || ''}`}
+                                      onInput={(event: React.FormEvent<HTMLInputElement>) => modifyCity(event)}/>
                     </FloatingLabel>
                 </Col>
                 <Col className=''>
                     <FloatingLabel label="Check in date">
-                        <Form.Control className='input' placeholder=' ' type="date" value={checkInDate} onInput={(event: React.FormEvent<HTMLInputElement>) => setCheckInDate(event.currentTarget.value)} />
+                        <Form.Control className='input' placeholder=' ' type="date" value={checkInDate}
+                                      onInput={(event: React.FormEvent<HTMLInputElement>) => setCheckInDate(event.currentTarget.value)}/>
                     </FloatingLabel>
                     <FloatingLabel label="Check out date">
-                        <Form.Control className='input' placeholder=' ' type="date" value={checkOutDate} onInput={(event: React.FormEvent<HTMLInputElement>) => setCheckOutDate(event.currentTarget.value)} />
+                        <Form.Control className='input' placeholder=' ' type="date" value={checkOutDate}
+                                      onInput={(event: React.FormEvent<HTMLInputElement>) => setCheckOutDate(event.currentTarget.value)}/>
                     </FloatingLabel>
                 </Col>
             </Row>
 
             <FloatingLabel label="Add description for your vacation... (max 1500 words)">
                 <Form.Control className='input' placeholder=' ' type='text'
-                    as='textarea' value={vacationDescription} onInput={(event: React.FormEvent<HTMLInputElement>) => setVacationDescription(event.currentTarget.value)} />
+                              as='textarea' value={vacationDescription}
+                              onInput={(event: React.FormEvent<HTMLInputElement>) => setVacationDescription(event.currentTarget.value)}/>
             </FloatingLabel>
 
             <Button className='button' variant="primary" type="submit"
-                onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => onButtonClick(event)}>
+                    onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => onButtonClick(event)}>
                 Submit
             </Button>
         </Form>
