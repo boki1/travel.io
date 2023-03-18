fe951bca1f6cb99bf8957524cd93074f4088183f
StoyanTinchev <st8tinchev@gmail.com>
2023-03-18 15:05:00 +0200

    Issue: #28 Edit: Multithreaded run configuration -- use env variables

--- platform/analyzer/run.py
+++ platform/analyzer/run.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), 'props.env'))
 
 workers = os.cpu_count()
-threads = 2  # TODO: undergoing customization - probably increase?
+threads = os.getenv('FLASK_RUN_THREADS', 2)
 subprocess.run(["gunicorn", "-w", str(workers),
                 "--threads", str(threads),
                 "-b", os.getenv('FLASK_RUN_HOST_PORT', '0.0.0.0:5005'),
