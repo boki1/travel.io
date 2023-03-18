a9ea8e3bdb7c36429c4b11b07411e42525fc421f
AntonioK180 <antonio.kasabov@gmail.com>
2023-03-18 15:47:04 +0200

    Add errorHandling in AnalyzerService.

--- platform/comm/src/main/java/com/fmicodes/comm/services/AnalyzerService.java
+++ platform/comm/src/main/java/com/fmicodes/comm/services/AnalyzerService.java
@@ -1,12 +1,12 @@
 package com.fmicodes.comm.services;
 
+import com.fmicodes.comm.exceptions.DeserializingJSONException;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.Response;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -24,50 +24,44 @@ public class AnalyzerService {
         AsyncHttpClient client = new DefaultAsyncHttpClient();
         String analyzedMessageResponse = null;
         try {
-            Response response = client.prepare("POST",  "http://" + analyzerHost + "/api/v1/analyzer")
+            Response response = client.prepare("POST", "http://" + analyzerHost + "/api/v1/analyzer")
                     .setBody(message)
                     .execute()
                     .get();
 
             analyzedMessageResponse = response.getResponseBody();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException("ERROR - Communicating with Flask API: " + e.getMessage());
         }
 
         JSONObject analyzedMessageJSON;
         try {
             analyzedMessageJSON = new JSONObject(analyzedMessageResponse);
         } catch (JSONException e) {
-            throw new RuntimeException(e);
+            throw new DeserializingJSONException("ERROR - Deserializing analyzedMessageResponse " + e.getMessage());
         }
 
         JSONArray locationsJSONArray;
         try {
             locationsJSONArray = analyzedMessageJSON.getJSONArray("locations");
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
 
-        for (int i = 0; i < locationsJSONArray.length(); i++) {
+            for (int i = 0; i < locationsJSONArray.length(); i++) {
 
-            JSONArray locationJSON;
-            try {
+                JSONArray locationJSON;
                 locationJSON = locationsJSONArray.getJSONArray(i);
                 String city = locationJSON.getString(1);
 
                 System.out.println("CITY: " + city);
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
             }
 
+        } catch (JSONException e) {
+            throw new DeserializingJSONException("ERROR - Deserializing locationsJSONArray: " + e.getMessage());
         }
 
         try {
             client.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("ERROR - Closing AsyncHttpClient: " + e.getMessage());
         }
 
         return analyzedMessageResponse;
