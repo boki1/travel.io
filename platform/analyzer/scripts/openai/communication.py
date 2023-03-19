0f134049eb38b761fdba74cf7035de4e7feb808b
StoyanTinchev <st8tinchev@gmail.com>
2023-03-19 03:24:58 +0200

    Issue: #48 Edit: Communication errors fix

--- platform/analyzer/scripts/openai/communication.py
+++ platform/analyzer/scripts/openai/communication.py
@@ -1,7 +1,7 @@
 import os
+
 import requests
 from flask import json, request
-
 from scripts.config import g_openai_hints
 
 
@@ -24,47 +24,45 @@ class OpenAICommunication:
                 Make sure to put corresponding closing tags.
             """
 
+    def get_openai_answer(self, question):
+        if not question:
+            return None
 
-def get_openai_answer(self, question):
-    if not question:
+        openai_api_key = os.getenv('OPENAI_SECRET_KEY', 'default-secret-key')
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {openai_api_key}",
+        }
+
+        # Hint OpenAI in order to omit unnecessary data processing of the response.
+        question += OpenAICommunication.hint()
+
+        data = {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": question}],
+            "temperature": 0,
+        }
+
+        response = requests.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers=headers,
+            data=json.dumps(data),
+        )
+
+        if response.status_code == 200:
+            response_data = response.json()
+            answer = response_data["choices"][0]["message"]["content"]
+            return answer
+
         return None
 
-    openai_api_key = os.getenv('OPENAI_SECRET_KEY', 'default-secret-key')
+    def handle_request(self):
+        question = request.get_data(as_text=True)
 
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {openai_api_key}",
-    }
+        answer = self.get_openai_answer(question)
 
-    # Hint OpenAI in order to omit unnecessary data processing of the response.
-    question += OpenAICommunication.hint()
+        if not answer or not question:
+            return "", ""
 
-    data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": question}],
-        "temperature": 0.2,
-    }
-
-    response = requests.post(
-        "https://api.openai.com/v1/chat/completions",
-        headers=headers,
-        data=json.dumps(data),
-    )
-
-    if response.status_code == 200:
-        response_data = response.json()
-        answer = response_data["choices"][0]["message"]["content"]
-        return answer
-
-    return None
-
-
-def handle_request(self):
-    question = request.get_data(as_text=True)
-
-    answer = self.get_openai_answer(question)
-
-    if not answer or not question:
-        return "", ""
-
-    return question, answer
+        return question, answer
